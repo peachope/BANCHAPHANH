a91ebf7d6d02f3e6f36ef68a03cd9a3f4679aebd
Hop Tran <hop.tran@successsoftware.global>
2021-10-10 14:16:32 +0700

    alo

--- js/audio.js
+++ js/audio.js
@@ -1,1 +1,16 @@
+var yourAudio = document.getElementById("yourAudio"),
+  ctrl = document.getElementById("audioControl");
 
+ctrl.onclick = function () {
+  // Update the Button
+
+  var pause = ctrl.innerHTML === "pause";
+  ctrl.innerHTML = pause ? "play" : "pause";
+
+  // Update the Audiov
+  var method = pause ? "pause" : "play";
+  yourAudio[method]();
+
+  // Prevent Default Action
+  return false;
+};
